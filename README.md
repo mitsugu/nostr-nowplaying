6b07eae46640cfc17c85385151cfc54b4b04699d
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2024-07-16 12:28:16 +0900

    first import

---
+++ README.md
@@ -0,0 +1,21 @@
+# nostr-nowplaying
+
+Update #nowplaying NIP-38 status (kind 30315) for Rhythmbox
+
+## Usage
+
+```
+$ ./nostr-nowplaying.sh
+```
+
+## Requirements
+
+* [algia](https://github.com/mattn/algia)
+
+## License
+
+MIT
+
+## Author
+
+Yasuhiro Matsumoto (a.k.a. mattn)
---
+++ nostr-nowplaying.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function sendMusicStatus() {
+  title=$(rhythmbox-client --print-playing)
+  if [ "$title" == " - " ]; then
+    return
+  fi
+  title_enc=$(echo $title | jq -Rr '@uri')
+  expiration=$(date -d'+5minutes' +%s)
+  algia event --kind 30315 --content "#NowPlaying $title" --tag d=music --tag expiration="$expiration" --tag r="spotify:search:$title_enc"
+}
+
+export -f sendMusicStatus
+
+dbus-monitor --session "path=/org/mpris/MediaPlayer2,member=PropertiesChanged" --monitor |\
+  stdbuf -i0 -oL grep PlaybackStatus |\
+  xargs -i{} bash -c sendMusicStatus
